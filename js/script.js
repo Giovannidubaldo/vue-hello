e2cee92ef4c3ba492502bed30557ef3905c36d4f
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-12-11 14:49:42 +0100

    Created Vue's structure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Definisco il metodo di Vue
+const { createApp } = Vue;
+
+// Creo la struttura per l'applicazione di Vue
+createApp({
+    data(){
+        return {
+
+        }
+    }
+}).mount('#app');
